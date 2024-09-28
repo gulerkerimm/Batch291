ab162efab3075d7f6354781771880555598933b4
gulerkerimm <gulerkerimm@gmail.com>
2024-09-28 17:53:01 +0300

    123

--- src/main/java/d14multidimensionalarrays_arraylist/Md01.java
+++ src/main/java/d14multidimensionalarrays_arraylist/Md01.java
@@ -5,42 +5,68 @@ import java.util.Arrays;
 public class Md01 {
     public static void main(String[] args) {
 
-        //Ornek 1: Iki boyutlu bir Array'i tek boyutlu bir Array'e ceviriniz.
-        //Example 1: Convert a two-dimensional Array to a one-dimensional Array.
-        // int[][] numbers = {{5, 4}, {2, 3, 2}}; ==> { 5,4,2,3,2 }
+        //Cok boyutlu dizi (MultiDimensional Array) temel olarak Array’in içine Array koymak anlamına gelir.
+        // En yaygın örneği iki boyutlu dizilerdir, ki bunlar genellikle matris olarak düşünülebilir.
+        // İki boyutlu bir dizi, satır ve sütunlardan oluşur, tıpkı bir Excel tablosu gibi.
 
-        //-----------------------
-        //1.adim: Iki boyutlu bir Array'de kac eleman oldugunu bulan kodu yazalim
-        int[][] numbers = {{5, 4}, {2, 3, 2}};
-        int totalNumberOfElement = 0;
+        //MultiDimensional Array nasil olusturulur?
+        int[][] a = new int[3][2]; //3 distaki, 2 icteki eleman sayisi
 
-        for (int[] w : numbers) {
-            totalNumberOfElement = totalNumberOfElement + w.length;
+        //MultiDimensional Array'lere nasil eleman eklenir?
+        a[0][0] = 5;
+        a[0][1] = 12;
+        a[1][0] = 81;
+        a[1][1] = 45;
+        a[2][0] = 123;
+        a[2][1] = 0;
+
+        //MultiDimensional Array konsola nasil yazdirilir?
+        System.out.println(Arrays.toString(a)); //[[I@4617c264, [I@36baf30c, [I@7a81197d]
+
+        //MultiDimensional Array'leri yazdirirken toString() degil, deepToString() method'u kullaniriz
+        System.out.println(Arrays.deepToString(a)); //[[5, 12], [81, 45], [123, 0]]
+
+        //MultiDimensional Array'den spesifik bir eleman nasil yazdirilir?
+        System.out.println(a[1][1]); //45
+
+        //MultiDimensional Array icindeki bir Array nasil yazdirilir?
+        System.out.println(Arrays.toString(a[2])); //[123, 0]
+
+        //Kisa yoldan MultiDimensional Array nasil olusturulur?
+        String[][] students = {{"Ali", "Kemal"}, {"Cemal"}, {"Ayhan", "Beyhan", "Seyhan"}, {"Ceyhan", "Burhan"}};
+        System.out.println(Arrays.deepToString(students));
+        //[[Ali, Kemal], [Cemal], [Ayhan, Beyhan, Seyhan], [Ceyhan, Burhan]]
+
+        //Ornek 1: Yukaridaki students array'inde toplam kac isim oldugunu bulunuz.
+        //Example 1: Find the total number of names in the students array above.
+
+        int sum = 0;
+
+        //[[Ali, Kemal], [Cemal], [Ayhan, Beyhan, Seyhan], [Ceyhan, Burhan]]
+        for (String[] w : students) {
+
+            sum = sum + w.length;
+
         }
-        System.out.println(totalNumberOfElement); //5
+        System.out.println(sum); //8
 
-        //-----------------------
-        //2.adim: Tek boyutlu Array'i, iki boyutlu Array'in eleman sayisini kullanarak olusturalim
-        int[] newArr = new int[totalNumberOfElement];
-        System.out.println(Arrays.toString(newArr)); //[0, 0, 0, 0, 0]
-        //-----------------------
-        //3.adim: Iki boyutlu Array'deki elemanlari tek boyutlu Array'e transfer edelim
-        int idx = 0;
+        //------------------------
+        //Ornek 2: Yukaridaki students Array'inde icinde "m" olan isimleri console'a yazdiriniz.
+        //Example 2: Write the names with "m" in the students Array to the console
 
 
-        //{{5, 4}, {2, 3, 2}};
-        for (int[] w : numbers) {
-            for (int k : w) {
-                newArr[idx] = k;
-                idx++;
+        //[[Ali, Kemal], [Cemal], [Ayhan, Beyhan, Seyhan], [Ceyhan, Burhan]]
+        for (String[] w : students) {
+            for (String k : w) {
+                if (k.contains("m")) {
+                    System.out.println(k);  //Kemal
+                    //Cemal
+                }
             }
         }
-        System.out.println(Arrays.toString(newArr)); //[5, 4, 2, 3, 2]
 
 
     }
 }
-
-
 
 
